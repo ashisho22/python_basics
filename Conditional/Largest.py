3e21dff8af94e6993164bdc55e0416bb013a2451
Ashish Ojha <ashishojha2205@gmail.com>
2026-04-21 19:56:18 +0530

    Create Largest.py

---
+++ Conditional/Largest.py
@@ -0,0 +1,9 @@
+Num1 = int(input("Write a number = "))
+Num2 = int(input("Write a number = "))
+
+if Num1 > Num2:
+    print(f"{Num1} is larger than {Num2}")
+elif Num2 > Num1:
+    print(f"{Num2} is larger than {Num1}")
+else:
+    print("Both numbers are equal")
